7c64f57aeee9da7ab290098615e8537b2a0eff5e
pdhaval <dhavalpatel738@gmail.com>
2016-11-01 12:32:03 +0530

    Initiating app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: '<h1>Initiating Digital Resume ...</h1><h2>{{title}}</h2>'
 })
-export class AppComponent { }
+
+export class AppComponent {
+ title = 'hi! there';
+}
